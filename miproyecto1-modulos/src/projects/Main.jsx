e0809e8b05d578938f110788884b7445de66a5aa
David <juandavidmolina0622@gmail.com>
2023-05-06 14:54:40 -0500

    Primer commit

---
+++ miproyecto1-modulos/src/projects/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Main = () => {
+    return (
+        <>
+            <main className='main-container'>
+
+                <div className="title-container">
+                    <h1 className='main-header'>David Molina</h1>
+                </div>
+
+
+                <div className="p-container">
+                    <p className='main-paragraph'> "La pasión en la programación es el combustible  <br />
+                        que impulsa la creación de soluciones innovadoras.".</p>
+                </div>
+            </main>
+        </>
+    )
+}
---
+++ miproyecto1/src/projects/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <>
+            <header className='first-header'>
+                <nav>
+                    <ul className='header-list'>
+                        <li><a href="xd1">Home</a></li>
+                        <li><a href="xd1">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
